de623feed76e1414c58b9cf174774182d634d40b
ritikkumar352 <krritik2002@gmail.com>
2025-01-28 00:55:57 +0530

    Nav bar initial stuct

---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,21 @@
+import './NavBar.css'
+function NavBar(){
+    return (
+        <>
+        <nav>
+            <ul>
+                <li><img src="" alt="akjsda" /></li>
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+                <li>D</li>
+                <li>E</li>
+            </ul>
+        </nav>
+        </>
+
+
+    )
+}
+
+export default NavBar;
