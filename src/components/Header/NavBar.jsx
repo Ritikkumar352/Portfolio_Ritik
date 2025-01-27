4902f1b6b91d39eae46a33100d03a34bc20f990a
ritikkumar352 <krritik2002@gmail.com>
2025-01-28 02:29:43 +0530

    working nav bar

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,18 +1,31 @@
 import './NavBar.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
 function NavBar(){
     return (
         <>
+        {/* <Router> */}
         <nav>
             <ul>
                 <li><img src="" alt="akjsda" /></li>
-                <li>A</li>
-                <li>B</li>
-                <li>C</li>
-                <li>D</li>
-                <li>E</li>
+                <li className='navLink'>A</li>
+                <li className='navLink'>B</li>
+                <li className='navLink'>C</li>
+                <li className='navLink'>D</li>
+                <li className='navLink'>E</li>
             </ul>
         </nav>
+
+
+        {/* </Router> */}
+
+        {/* <Routes>
+
+        </Routes> */}
+
         </>
+
+
 
 
     )
